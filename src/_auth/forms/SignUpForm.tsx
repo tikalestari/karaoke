26e076ef4a727846645ff506faecf45767fe669a
tikalestari <tika.dwilestari@gmail.com>
2024-01-26 16:08:54 -0800

    Add loader and text to sign in if already have an account

--- src/_auth/forms/SignUpForm.tsx
+++ src/_auth/forms/SignUpForm.tsx
@@ -1,14 +1,16 @@
 import * as z from "zod"
 import { zodResolver } from "@hookform/resolvers/zod"
-
+import { Link } from "react-router-dom"
 import { Button } from "@/components/ui/button"
 import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 
 import { useForm } from "react-hook-form"
 import { SignUpValidation } from "@/lib/validation"
+import Loader from "@/components/shared/Loader"
 
 const SignUpForm = () => {
+  const isLoading = false;
   // 1. Define your form.
   const form = useForm<z.infer<typeof SignUpValidation>>({
     resolver: zodResolver(SignUpValidation),
@@ -31,7 +33,7 @@ const SignUpForm = () => {
       <div className="sm:w-420 flex-center flex-col">
         {/* <img src="" /> */}
         <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">Create a new account</h2>
-        <p className="text-light-3 small-medium md:base-regular mt-2">To use karaoke, enter your details</p>
+        <p className="text-light-3 small-medium md:base-regular mt-2">To use karaoke, please enter your details</p>
 
         <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-5 w-full mt-4">
           <FormField
@@ -89,7 +91,17 @@ const SignUpForm = () => {
               </FormItem>
             )}
           />
-          <Button type="submit">Submit</Button>
+          <Button type="submit" className="shad-button_primary">
+            {isLoading ? (
+              <div className="flex-center gap-2">
+                <Loader/> Loading...
+              </div>
+            ): "Sign up"}
+          </Button>
+          <p className="text-small-regular text-light-2 text-center mt-2">
+            Already have an account?
+            <Link to="/sign-in" className="text-primary-500 text-small-semibold ml-1"> Log in</Link>
+          </p>
         </form>
       </div>
     </Form>
