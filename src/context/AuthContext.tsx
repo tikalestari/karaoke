7c39dea3c50e038bf7ed5f1187bd04af561b9d50
tikalestari <tika.dwilestari@gmail.com>
2024-01-30 17:17:10 -0800

    Implement sign in page

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -67,8 +67,8 @@ const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   };
 
   useEffect(() => {
-    // localStorage.getItem('cookieFallback') === null
     if (
+      localStorage.getItem('cookieFallback') === null ||
       localStorage.getItem('cookieFallback') === '[]'
       ) {
         navigate('/sign-in')
